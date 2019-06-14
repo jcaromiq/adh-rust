f1a33e63b6d1a966fe04ab711e4a4cbf6a63a443
Joaquin Caro <jcaromiq@gmail.com>
2019-06-14 22:52:11 +0200

    add format

--- src/domain/container.rs
+++ src/domain/container.rs
@@ -1,4 +1,4 @@
-use prettytable::Table;
+use prettytable::{Table, format};
 
 pub struct Containers {
     list: Vec<Container>
@@ -15,34 +15,45 @@ struct Container {
 impl Containers {
     pub fn print(&self) {
         let mut table = Table::new();
-        table.add_row(row!["CONTAINER ID", "NAMES", "IMAGE", "STATUS", "PORTS"]);
+        let format = format::FormatBuilder::new()
+            .borders('|')
+            .separators(&[format::LinePosition::Top],
+                        format::LineSeparator::new('─', ' ', '┌', '┐'))
+            .separators(&[format::LinePosition::Bottom],
+                        format::LineSeparator::new('─', ' ', '└', '┘'))
+            .padding(4, 4)
+            .build();
+        table.set_format(format);
+        table.set_titles(row![bc=> "CONTAINER ID", "NAMES", "IMAGE", "STATUS", "PORTS"]);
         for c in &self.list {
-            table.add_row(row![&c.id, c.name, c.image, c.status, c.ports]);
+            table.add_row(row![c.id, c.name, c.image, c.status, c.ports]);
         }
         table.printstd();
     }
 }
 
 pub fn to_domain(containers: Vec<shiplift::rep::Container>) -> Containers {
-    let mut container_list: Vec<Container> = Vec::new();
+    let mut list: Vec<Container> = Vec::new();
     for c in containers {
+        let id = c.id[0..12].to_string();
+        let name = c.names[0].to_string();
         let mut image = c.image.to_string();
+        let status = c.status;
         if image.contains("sha256:") {
             image = c.image[7..19].to_string();
         }
-        let mut ports = String::from("");
+        let mut ports = "".to_string();
         if !c.ports.is_empty() {
+            //FIX: iterate over port
             ports = format!("0.0.0.0:{}->{}/{}", c.ports[0].public_port.unwrap(), c.ports[0].private_port, c.ports[0].typ);
         }
-        container_list.push(Container {
-            id: c.id[0..12].to_string(),
-            name: c.names[0].to_string(),
+        list.push(Container {
+            id,
+            name,
             image,
-            status: c.status,
+            status,
             ports,
-        });
+        })
     }
-    return Containers {
-        list: container_list
-    };
+    return Containers { list };
 }
