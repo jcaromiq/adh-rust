c56e3c6332a251daa7452739656f3405027e9f84
Joaquin Caro <jcaromiq@gmail.com>
2019-06-14 16:30:23 +0200

    refactor: extract print table

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,6 @@ extern crate prettytable;
 
 use prettytable::Table;
 
-struct ContainerInfo {
-    id: String,
-    name: String,
-    image: String,
-    status: String,
-    ports: String,
-}
 
 fn main() {
     let matches = App::new("ApiumDockerHelper")
@@ -34,8 +27,6 @@ fn main() {
     if let Some(_) = matches.subcommand_matches("psa") {
         psa(Docker::new());
     }
-
-
 }
 
 pub fn psa(docker: Docker) {
@@ -43,24 +34,7 @@ pub fn psa(docker: Docker) {
         .containers()
         .list(&ContainerListOptions::builder().all().build())
         .map(|containers| {
-            let mut table = Table::new();
-
-            table.add_row(row!["CONTAINER ID", "NAMES", "IMAGE", "STATUS", "PORTS"]);
-            for c in containers {
-                let mut image = c.image.to_string();
-                if image.contains("sha256:") {
-                    println!("si!");
-                    image = c.image[7..19].to_string();
-                }
-                if !c.ports.is_empty() {
-                    let port = format!("0.0.0.0:{}->{}/{}", c.ports[0].public_port.unwrap(), c.ports[0].private_port, c.ports[0].typ);
-                    table.add_row(row![&c.id[0..12], c.names[0], image, c.status, &port]);
-                } else {
-                    table.add_row(row![&c.id[0..12], c.names[0], image, c.status, ""]);
-                }
-
-            }
-            table.printstd();
+            print(containers);
         })
         .map_err(|e| eprintln!("Error: {}", e));
 
@@ -72,25 +46,29 @@ pub fn ps(docker: Docker) {
         .containers()
         .list(&Default::default())
         .map(|containers| {
-            let mut table = Table::new();
-
-            table.add_row(row!["CONTAINER ID", "NAMES", "IMAGE", "STATUS", "PORTS"]);
-            for c in containers {
-                let mut image = c.image.to_string();
-                if image.contains("sha256:") {
-                    image = c.image[7..19].to_string();
-                }
-                if !c.ports.is_empty() {
-                    let port = format!("0.0.0.0:{}->{}/{}", c.ports[0].public_port.unwrap(), c.ports[0].private_port, c.ports[0].typ);
-                    table.add_row(row![&c.id[0..12], c.names[0], image, c.status, &port]);
-                } else {
-                    table.add_row(row![&c.id[0..12], c.names[0], image, c.status, ""]);
-                }
-
-            }
-            table.printstd();
+            print(containers);
         })
         .map_err(|e| eprintln!("Error: {}", e));
 
     tokio::run(fut);
+}
+
+fn print(containers: Vec<shiplift::rep::Container>) {
+    let mut table = Table::new();
+
+    table.add_row(row!["CONTAINER ID", "NAMES", "IMAGE", "STATUS", "PORTS"]);
+    for c in containers {
+        let mut image = c.image.to_string();
+        if image.contains("sha256:") {
+            println!("si!");
+            image = c.image[7..19].to_string();
+        }
+        if !c.ports.is_empty() {
+            let port = format!("0.0.0.0:{}->{}/{}", c.ports[0].public_port.unwrap(), c.ports[0].private_port, c.ports[0].typ);
+            table.add_row(row![&c.id[0..12], c.names[0], image, c.status, &port]);
+        } else {
+            table.add_row(row![&c.id[0..12], c.names[0], image, c.status, ""]);
+        }
+    }
+    table.printstd();
 }
