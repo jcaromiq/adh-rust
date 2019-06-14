0fab3842b0c8ccdfdb27a38484c849380ec6a54e
Joaquin Caro <jcaromiq@gmail.com>
2019-06-14 16:21:42 +0200

    psa command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use shiplift::{Docker};
+use shiplift::{Docker, ContainerListOptions};
 use tokio::prelude::Future;
-#[macro_use] extern crate prettytable;
-use prettytable::{Table};
+use clap::{App, SubCommand};
+
+#[macro_use]
+extern crate prettytable;
+
+use prettytable::Table;
 
 struct ContainerInfo {
     id: String,
@@ -12,7 +16,58 @@ struct ContainerInfo {
 }
 
 fn main() {
-    let docker = Docker::new();
+    let matches = App::new("ApiumDockerHelper")
+        .version("0.1.0")
+        .author("Joaco <me@joaquin-caro.es>")
+        .about("adh")
+        .subcommand(SubCommand::with_name("ps").about("Formatted ps for running dockers"))
+        .subcommand(SubCommand::with_name("psa").about("Formatted ps for all dockers"))
+//        .subcommand(SubCommand::with_name("deploy")
+//            .arg(Arg::with_name("serviceName").index(1).required(true))
+//            .arg(Arg::with_name("stage").index(2).required(true))
+//            .arg(Arg::with_name("version").value_name("version").required(true).short("v")))
+        .get_matches();
+
+    if let Some(_) = matches.subcommand_matches("ps") {
+        ps(Docker::new());
+    }
+    if let Some(_) = matches.subcommand_matches("psa") {
+        psa(Docker::new());
+    }
+
+
+}
+
+pub fn psa(docker: Docker) {
+    let fut = docker
+        .containers()
+        .list(&ContainerListOptions::builder().all().build())
+        .map(|containers| {
+            let mut table = Table::new();
+
+            table.add_row(row!["CONTAINER ID", "NAMES", "IMAGE", "STATUS", "PORTS"]);
+            for c in containers {
+                let mut image = c.image.to_string();
+                if image.contains("sha256:") {
+                    println!("si!");
+                    image = c.image[7..19].to_string();
+                }
+                if !c.ports.is_empty() {
+                    let port = format!("0.0.0.0:{}->{}/{}", c.ports[0].public_port.unwrap(), c.ports[0].private_port, c.ports[0].typ);
+                    table.add_row(row![&c.id[0..12], c.names[0], image, c.status, &port]);
+                } else {
+                    table.add_row(row![&c.id[0..12], c.names[0], image, c.status, ""]);
+                }
+
+            }
+            table.printstd();
+        })
+        .map_err(|e| eprintln!("Error: {}", e));
+
+    tokio::run(fut);
+}
+
+pub fn ps(docker: Docker) {
     let fut = docker
         .containers()
         .list(&Default::default())
@@ -21,8 +76,16 @@ fn main() {
 
             table.add_row(row!["CONTAINER ID", "NAMES", "IMAGE", "STATUS", "PORTS"]);
             for c in containers {
-                let p = format!("0.0.0.0:{}->{}/{}",c.ports[0].public_port.unwrap(),c.ports[0].private_port,c.ports[0].typ);
-                table.add_row(row![c.id, c.names[0], c.image, c.status, p]);
+                let mut image = c.image.to_string();
+                if image.contains("sha256:") {
+                    image = c.image[7..19].to_string();
+                }
+                if !c.ports.is_empty() {
+                    let port = format!("0.0.0.0:{}->{}/{}", c.ports[0].public_port.unwrap(), c.ports[0].private_port, c.ports[0].typ);
+                    table.add_row(row![&c.id[0..12], c.names[0], image, c.status, &port]);
+                } else {
+                    table.add_row(row![&c.id[0..12], c.names[0], image, c.status, ""]);
+                }
 
             }
             table.printstd();
@@ -30,6 +93,4 @@ fn main() {
         .map_err(|e| eprintln!("Error: {}", e));
 
     tokio::run(fut);
-
-
 }
