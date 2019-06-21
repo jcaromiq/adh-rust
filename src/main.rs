511507d7b0091b6791554d5387a7dc190a4e164e
Joaquin Caro <jcaromiq@gmail.com>
2019-06-21 14:05:53 +0200

    change display order of commands

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,27 @@ fn main() {
         .version("0.1.0")
         .author("Joaco <me@joaquin-caro.es>")
         .about("docker helper")
-        .subcommand(SubCommand::with_name("nginx").about("Run nginx with a volume in the current directory"))
+        .subcommand(SubCommand::with_name("nginx")
+            .about("Run nginx with a volume in the current directory")
+            .display_order(0))
         .subcommand(SubCommand::with_name("start")
             .about("Start containers")
-            .arg(Arg::with_name("options")
-                .help("print debug information verbosely")
+            .display_order(1)
+            .arg(Arg::with_name("container_id")
+                .help("container id to start")
                 .required(true)))
         .subcommand(SubCommand::with_name("stop")
             .about("Stop containers")
-            .arg(Arg::with_name("options")
-                .help("print debug information verbosely")
+            .display_order(2)
+            .arg(Arg::with_name("container_id")
+                .help("container id to stop")
                 .required(true)))
-        .subcommand(SubCommand::with_name("ps").about("Formatted ps for running dockers"))
-        .subcommand(SubCommand::with_name("psa").about("Formatted ps for all dockers"))
+        .subcommand(SubCommand::with_name("ps")
+            .about("Formatted ps for running dockers")
+            .display_order(3))
+        .subcommand(SubCommand::with_name("psa")
+            .display_order(4)
+            .about("Formatted ps for all dockers"))
         .get_matches();
 
     if let Some(_) = matches.subcommand_matches("ps") {
@@ -39,9 +47,9 @@ fn main() {
         commands::nginx::execute();
     }
     if let Some(m) = matches.subcommand_matches("start") {
-        commands::start::execute(m.value_of("options").unwrap());
+        commands::start::execute(m.value_of("container_id").unwrap());
     }
     if let Some(m) = matches.subcommand_matches("stop") {
-        commands::stop::execute(m.value_of("options").unwrap());
+        commands::stop::execute(m.value_of("container_id").unwrap());
     }
 }
