76c6deb0f95f3e3e80bda30523d92676054c1891
Joaquin Caro <jcaromiq@gmail.com>
2019-07-24 18:15:40 +0200

    factory for remove-none-images

--- src/commands/command.rs
+++ src/commands/command.rs
@@ -3,7 +3,7 @@ use clap::ArgMatches;
 use crate::commands::nginx::Nginx;
 use crate::commands::ps::Ps;
 use crate::commands::psa::Psa;
-//use crate::commands::remove_none_images::RemoveNoneImages;
+use crate::commands::remove_none_images::RemoveNoneImages;
 
 pub trait Command {
     fn execute(&self);
@@ -22,8 +22,8 @@ pub fn factory(matches: ArgMatches) -> Box<Command> {
         return Box::new(Nginx);
     }
 
-//    if let Some(_) = matches.subcommand_matches("remove-none-images") {
-//        return Box::new(RemoveNoneImages);
-//    }
+    if let Some(_) = matches.subcommand_matches("remove-none-images") {
+        return Box::new(RemoveNoneImages);
+    }
     return Box::new(Psa);
 }
--- src/commands/remove_none_images.rs
+++ src/commands/remove_none_images.rs
@@ -1,42 +1,50 @@
-//use futures::Future;
-//use shiplift::{Docker, Error, ImageFilter, ImageListOptions};
-//use shiplift::rep::Image;
-//
-//use crate::commands::command::Command;
-//
-//pub struct RemoveNoneImages;
-//
-//impl RemoveNoneImages {
-//    fn delete(&self, images: Vec<Image>) -> std::result::Result<(), Error> {
-//        let docker = Docker::new();
-//        for image in images {
-//            let ff = docker.images()
-//                .get(image.id.as_str())
-//                .delete()
-//                .map(move |_| println!("deleted {}", image.id))
-//                .map_err(|e| eprintln!("Error: {} deleting container", e));
-//            tokio::spawn(ff);
-//        }
-//        Ok(())
-//    }
-//}
-//
-//impl Command for RemoveNoneImages {
-//    fn execute(&self) {
-//        let docker = Docker::new();
-//
-//        let dangling_filter = ImageListOptions::builder()
-//            .filter(vec!(ImageFilter::Dangling))
-//            .build();
-//
-//        let op = docker
-//            .images()
-//            .list(&dangling_filter)
-//            .and_then(move |images| {
-//                &self.delete(images)
-//            })
-//            .map(|_| println!("All none images deleted"))
-//            .map_err(|e| eprintln!("Error: {}", e));
-//        tokio::run(op);
-//    }
-//}
+use futures::Future;
+use shiplift::{Docker, Error, ImageFilter, ImageListOptions};
+use shiplift::rep::Image;
+
+use crate::commands::command::Command;
+
+pub struct RemoveNoneImages;
+
+impl RemoveNoneImages {
+    fn delete(&self, images: Vec<Image>) -> std::result::Result<(), Error> {
+        let docker = Docker::new();
+        for image in images {
+            let ff = docker.images()
+                .get(image.id.as_str())
+                .delete()
+                .map(move |_| println!("deleted {}", image.id))
+                .map_err(|e| eprintln!("Error: {} deleting container", e));
+            tokio::spawn(ff);
+        }
+        Ok(())
+    }
+}
+
+impl Command for RemoveNoneImages {
+    fn execute(&self) {
+        let docker = Docker::new();
+
+        let dangling_filter = ImageListOptions::builder()
+            .filter(vec!(ImageFilter::Dangling))
+            .build();
+
+        let op = docker
+            .images()
+            .list(&dangling_filter)
+            .and_then(move |images| {
+                for image in images {
+                    let ff = docker.images()
+                        .get(image.id.as_str())
+                        .delete()
+                        .map(move |_| println!("deleted {}", image.id))
+                        .map_err(|e| eprintln!("Error: {} deleting container", e));
+                    tokio::spawn(ff);
+                }
+                Ok(())
+            })
+            .map(|_| println!("All none images deleted"))
+            .map_err(|e| eprintln!("Error: {}", e));
+        tokio::run(op);
+    }
+}
