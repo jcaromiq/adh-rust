cc697169cf99c581a75c1fa950a5d6c2b0ae5ebd
Joaquin Caro <jcaromiq@gmail.com>
2019-07-25 10:50:52 +0200

    test example

--- tests/main.rs
+++ tests/main.rs
@@ -3,29 +3,7 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 5);
+    fn green() {
+        assert_eq!(2 + 2, 4);
     }
-}
-
-trait DockerCommand {
-    fn execute(&self);
-}
-
-struct PSCommand {
-    args: String,
-}
-
-impl DockerCommand for PSCommand {
-    fn execute(&self) {
-        println!("{}", self.args);
-    }
-}
-
-#[test]
-fn m() {
-    let p: DockerCommand = PSCommand {
-        args: "a".to_string()
-    };
-    p.execute();
 }
