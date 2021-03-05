07363c8ecb76777c7387518c805659ee0869be5b
Joaquin Caro <jcaromiq@gmail.com>
2021-03-05 18:09:39 +0100

    fix volume creation nginx

--- src/commands/nginx.rs
+++ src/commands/nginx.rs
@@ -3,15 +3,18 @@ use shiplift::ContainerOptions;
 use crate::commands::command::Command;
 use crate::commands::create_and_run::create_and_run;
 use async_trait::async_trait;
+use std::env;
 
 pub struct Nginx;
 
 #[async_trait]
 impl Command for Nginx {
     async fn execute(&self) {
+        let volume = format!("{}:/usr/share/nginx/html", env::current_dir().unwrap().display());
         let options = &ContainerOptions::builder("nginx")
             .name("adh-nginx")
             .expose(80, "tcp", 8888)
+            .volumes(vec![&volume])
             .build();
         create_and_run(options, "nginx:latest").await;
     }
