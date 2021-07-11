7f504e7fb624c42c8b9afbaab038a42ad718a1d6
Joaquin Caro <jcaromiq@gmail.com>
2021-07-11 20:13:28 +0200

    iterate over ports

--- src/domain/container.rs
+++ src/domain/container.rs
@@ -1,11 +1,13 @@
 pub struct Containers {
     pub list: Vec<Container>,
 }
+
 impl Containers {
     pub fn is_empty(&self) -> bool {
         self.list.is_empty()
     }
 }
+
 pub struct Container {
     pub id: String,
     pub name: String,
@@ -16,28 +18,30 @@ pub struct Container {
 
 pub fn to_domain(containers: Vec<shiplift::rep::Container>) -> Containers {
     let mut list: Vec<Container> = Vec::new();
-    for c in containers {
-        let id = c.id[0..12].to_string();
-        let name = c.names[0].to_string();
-        let mut image = c.image.to_string();
-        let status = c.status;
+    for container in containers {
+        let id = container.id[0..12].to_string();
+        let name = container.names[0].to_string();
+        let mut image = container.image.to_string();
+        let status = container.status;
         if image.contains("sha256:") {
-            image = c.image[7..19].to_string();
+            image = container.image[7..19].to_string();
         }
-        let mut ports = "".to_string();
-        if !c.ports.is_empty() {
-            //FIX: iterate over port
-            if c.ports[0].public_port.is_some() {
-                ports = format!(
-                    "0.0.0.0:{}->{}/{}",
-                    c.ports[0].public_port.unwrap(),
-                    c.ports[0].private_port,
-                    c.ports[0].typ
-                );
-            } else {
-                ports = format!("{}/{}", c.ports[0].private_port, c.ports[0].typ);
+        let mut public_ports = "".to_string();
+        let mut private_ports = "".to_string();
+        if !container.ports.is_empty() {
+            for port in container.ports {
+                if port.public_port.is_some() {
+                    public_ports.push_str(&format!(
+                        "0.0.0.0:{}->{}\n",
+                        port.public_port.unwrap(),
+                        port.private_port,
+                    ));
+                } else {
+                    private_ports.push_str(&format!("{} ", port.private_port))
+                }
             }
         }
+        let ports = public_ports + &private_ports;
         list.push(Container {
             id,
             name,
